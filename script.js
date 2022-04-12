79fd3ba2f59c7ef396224a3d4ed82d9e32bb2228
nadka :) <101661628+nadkka@users.noreply.github.com>
2022-04-12 17:04:49 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+var cards= ['kot.jpg','kot4.jpg','kot2.jpg','kot5.jpg','kot.jpg','kot3.jpg','kot6.jpg','kot3.jpg','kot6.jpg','kot2.jpg','kot4.jpg','kot5.jpg'];
+var oneVisible = false;
+var nrKarty;
+
+function odkryj(nr){
+var obraz= "url("+cards[nr]+")";
+
+
+document.getElementById('c'+ nr).style.backgroundImage=obraz;
+//$("#c" + nr).css("background-image", obraz );
+
+if(oneVisible==false){
+    //pierwsza karta
+
+    oneVisible=true;
+    nrKarty=nr;
+}
+else{
+    //druga karta
+    if(cards[nrKarty]==cards[nr]){
+        alert("PARA!");
+
+    }
+    else{
+       alert("PUDŁO!");
+    }
+}
+}
